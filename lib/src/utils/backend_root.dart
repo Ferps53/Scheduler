eb5bcf2f2085ac2a92c6e4b112b410a568e64461
Felipe Brostolin <felipe.brostolin53@gmail.com>
2023-12-27 20:09:43 -0300

    Alteração do endereço do servidor

--- lib/src/utils/backend_root.dart
+++ lib/src/utils/backend_root.dart
@@ -1,3 +1,4 @@
 class BackendRoot {
-  static String path = "https://placid-structure-production.up.railway.app";
+  static String path =
+      "https://scheduler-backend-production-805d.up.railway.app";
 }
