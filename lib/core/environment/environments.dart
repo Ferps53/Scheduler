75e2fae351a6691c0f68eb5f06ca630520754bb8
Felipe Brostolin <felipebrostolinribeiro@gmail.com>
2024-04-19 09:59:39 -0300

    remoção do ; no comentário

--- lib/core/environment/environments.dart
+++ lib/core/environment/environments.dart
@@ -2,6 +2,6 @@ class Environments {
 
     static String backendRoot = "http://localhost:8080/";
 
-    //TODO: Adicionar o backendRoot do Server;
+    //TODO: Adicionar o backendRoot do Server
     // static String backendRoot = "";
 }
