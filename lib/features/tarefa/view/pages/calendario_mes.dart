fb767e8b9c3cec4fbf0ee7de2b111b9e53e463f7
Felipe Brostolin <felipebrostolinribeiro@gmail.com>
2024-05-17 17:59:04 -0300

    começo do trabalho com o widget calendário

---
+++ lib/features/tarefa/view/pages/calendario_mes.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:scheduler/core/core.dart';
+
+class CalendarioMes extends StatelessWidget {
+  const CalendarioMes({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 14),
+      child: Center(
+        child: SizedBox(
+          height: 332,
+          child: GlassCard(
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  color: Theme.of(context).colorScheme.primary,
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _DiaCalendario(dia: "D"),
+                      _DiaCalendario(dia: "S"),
+                      _DiaCalendario(dia: "T"),
+                      _DiaCalendario(dia: "Q"),
+                      _DiaCalendario(dia: "Q"),
+                      _DiaCalendario(dia: "S"),
+                      _DiaCalendario(dia: "S"),
+                    ],
+                  ),
+                ),
+                const _GridDias(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _GridDias extends StatelessWidget {
+  const _GridDias();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.builder(
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 7),
+        itemCount: 31,
+        itemBuilder: (context, index) {
+          index++;
+          return index == 1
+              ? SizedBox(
+                  width: 10,
+                  height: 10,
+                )
+              : GridTile(
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Badge(
+                      child: GlassCard(
+                        startGradient: 0.5,
+                        endGradient: 0.3,
+                        child: Center(
+                          child: Text(
+                            "$index",
+                            style: TextStyle(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onPrimaryContainer,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+        },
+      ),
+    );
+  }
+}
+
+class _DiaCalendario extends StatelessWidget {
+  final String dia;
+  const _DiaCalendario({
+    required this.dia,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      dia,
+      style: TextStyle(
+          color: Theme.of(context).colorScheme.onPrimary, fontSize: 18),
+    );
+  }
+}
--- lib/features/tarefa/view/pages/tarefa_page.dart
+++ lib/features/tarefa/view/pages/tarefa_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:scheduler/core/core.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:scheduler/features/tarefa/view/pages/calendario_mes.dart';
 
 class TarefaPage extends StatelessWidget {
   const TarefaPage({super.key});
@@ -14,47 +13,8 @@ class TarefaPage extends StatelessWidget {
         foregroundColor: Theme.of(context).colorScheme.onPrimary,
         title: const Text('Tarefas'),
       ),
-      body: Column(
-        children: [
-          OutlinedButton(
-            onPressed: () async {
-              final prefs = await SharedPreferences.getInstance();
-              prefs.clear();
-              context.go(NamedRoutes.login.routePath);
-            },
-            child: const Text("Clear"),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3),
-                  itemCount: 30,
-                  itemBuilder: (context, index) {
-                    index++;
-                    return GridTile(
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: GlassCard(
-                          child: Center(
-                            child: Text(
-                              "$index",
-                              style: TextStyle(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onPrimaryContainer,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  }),
-            ),
-          )
-        ],
-      ),
+      body: const CalendarioMes(),
     );
   }
 }
+
